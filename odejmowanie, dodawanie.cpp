e7b09a5c487665bfcba85bc80dae17fea1bf7624
OscarWiliwis <90095559+OscarWiliwis@users.noreply.github.com>
2021-09-21 12:12:29 +0200

    Add files via upload

---
+++ odejmowanie, dodawanie.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+int a, b, c, d, e, f, g, h;
+void dodawanie(int a, int b) {
+	cout << a + b << endl;
+}
+void odejmowanie(int c, int d) {
+	cout << c - d << endl;
+}
+void mnozenie(int e, int f) {
+	cout << e * f << endl;
+}
+void dzielenie(int g, int h) {
+	cout << g / h << endl;
+}
+	int main()
+	{
+		dodawanie(1, 2);
+
+		odejmowanie(3, 4);
+
+		mnozenie(5, 6);
+
+		dzielenie(7, 8);
+	}
+
+   
